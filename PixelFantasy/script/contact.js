de5f621fda7ce0517b4ca980a688a69d23a78b09
zeki <zeki.chang@leadwins.com>
2022-06-27 15:55:02 +0800

    Add PixelFantsy WebSite

---
+++ PixelFantasy/script/contact.js
@@ -0,0 +1,11 @@
+function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (!regex.test(email)) {
+        getClasses("formTip")[0].innerHTML = "請填寫正確的郵件格式！";
+        
+    } else {
+        getClasses("formTip")[0].innerHTML = "";
+        return true;
+    }
+}
+
