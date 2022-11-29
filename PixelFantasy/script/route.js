00afe6ba2d37584a13362be1547600fe016f6068
Zeki <zekixfly@hotmail.com>
2022-11-29 19:18:37 +0800

    PixelFantasy fix mounScript function

--- PixelFantasy/script/route.js
+++ PixelFantasy/script/route.js
@@ -37,14 +37,16 @@ let route = {
             const htmlElement = htmlParser.parseFromString(htmlText, 'text/html');
 
             // 取得template樣板網頁的內容
-            const htmlTemplate = htmlElement.getTags('template')[0].innerHTML;
-
+            const template = htmlElement.getTags('template')[0];
+            const htmlTemplate = template.innerHTML;
             function mountScript(target){
-                /* 判斷template樣板網頁裡是否有script，
-                如果有script就取得template樣板網頁裡的script，
-                並將script載入template當前的樣板頁面。 */
-                if(htmlElement.getTags('script').length !== 0){
-                    htmlElement.getTags('script').map( script => {
+                /* 因為已掛載的template樣板網頁，並不會自動執行script標籤裡面的語法，
+                故在此判斷已掛載的template樣板網頁裡是否有script，
+                如果有script就取得已掛載網頁裡的script，
+                並將原本的script標籤刪除並重新載入scirpt執行。 */
+                if(document.querySelector(`[slot=${target}]`).getTags('script').length !== 0){
+                    [...document.querySelector(`[slot=${target}]`).getTags('script')].map( script => {
+                        document.querySelector(`[slot=${target}]`).delKid(script);
                         const scriptTag = makeTag('script');
                         scriptTag.type = script.type || 'text/javascript';
                         if(script.src){
@@ -54,7 +56,7 @@ let route = {
                         }
                         document.querySelector(`[slot=${target}]`).addKid(scriptTag);
                     })                
-                }                    
+                }
             }
 
             // 找尋插入的標籤
