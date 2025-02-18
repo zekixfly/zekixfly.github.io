a39099996dc0e2772e74a6200c7fc4275f79dde2
changhh <zekixfly@hotmail.com>
2025-02-18 11:53:00 +0800

    Fix Taiwan time zone for dates.js

--- site/_filters/dates.js
+++ site/_filters/dates.js
@@ -2,7 +2,9 @@ const dayjs = require("dayjs");
 
 dayjs
   .extend(require("dayjs/plugin/localizedFormat"))
-  .extend(require("dayjs/plugin/relativeTime"));
+  .extend(require("dayjs/plugin/relativeTime"))
+  .extend(require("dayjs/plugin/utc"))
+  .extend(require("dayjs/plugin/timezone"));
 
 const dayjsLocales = {
   // dayjs sometimes don't use the right format!
@@ -10,6 +12,7 @@ const dayjsLocales = {
   "en-GB": "en-gb",
   // 'en-US': 'en',
 };
+const tz = "Asia/Taipei";
 
 Object.values(dayjsLocales).forEach((locale) =>
   require(`dayjs/locale/${locale}`)
@@ -17,13 +20,13 @@ Object.values(dayjsLocales).forEach((locale) =>
 
 module.exports = {
   dateFormat: function (date, format = "LLL", locale = this.ctx.site.locale) {
-    return dayjs(date).locale(dayjsLocales[locale]).format(format);
+    return dayjs(date).locale(dayjsLocales[locale]).tz(tz).format(format);
   },
 
   dateRelative: function (date, locale = this.ctx.site.locale) {
-    return dayjs(date).locale(dayjsLocales[locale]).fromNow();
+    return dayjs(date).locale(dayjsLocales[locale]).tz(tz).fromNow();
   },
 
-  dateISO: (date) => dayjs(date).toISOString(),
-  dateRSS: (date) => dayjs(date).format("ddd, D MMM YYYY HH:mm:ss ZZ"),
+  dateISO: (date) => dayjs(date).tz(tz).toISOString(),
+  dateRSS: (date) => dayjs(date).tz(tz).format("ddd, D MMM YYYY HH:mm:ss ZZ"),
 };
